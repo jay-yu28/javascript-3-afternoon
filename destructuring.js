1b13488768255faec3004144087a3175d046208a
Jay Yu <jay.nam28@gmail.com>
2018-08-09 08:14:53 -0500

    Complete

--- destructuring.js
+++ destructuring.js
@@ -21,7 +21,10 @@ var carDetails = {
   Use object destructuring to save the property values from the object carDetails into new variables. 
 */
 
-//Code Here
+var {color} = carDetails
+var {make} = carDetails
+var {model} = carDetails
+var {year} = carDetails
 
 
 
@@ -34,7 +37,9 @@ var carDetails = {
 */
 
 function greeting( obj ) {
-  //Code Here
+  var {firstName} = obj
+  var {lastName} = obj
+  var {title} = obj
   
   // Do not edit the code below.
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
@@ -53,7 +58,13 @@ function greeting( obj ) {
   Sum up the values and return the total number.
 */
 
-//Code Here
+const totalPopulation = obj => {
+  var {utah} = obj
+  var {california} = obj
+  var {texas} = obj
+  var {arizona} = obj
+  return utah + california + texas + arizona
+}
 
 
 
@@ -67,7 +78,14 @@ function greeting( obj ) {
   Push these new variables to an array and return the array. 
 */
 
-//Code Here
+const ingredients = obj => {
+  var {carb} = obj
+  var {fat} = obj
+  var {protein} = obj
+  var newArr = []
+  newArr.push(carb, fat, protein)
+  return newArr
+}
 
 
 
@@ -85,7 +103,15 @@ function greeting( obj ) {
   Find the smallest number of the three and return that number.
 */
 
-//Code Here
+const largeNumbers = ({first, second, third}) => {
+ if(first < second && first < third){
+   return first
+ } else if(second < first && second < third){
+   return second
+ } else if(third < first && third < second){
+   return third
+ }
+}
 
 
 
@@ -97,6 +123,13 @@ function greeting( obj ) {
   Find the longest array and return that array.
 */
 
-//Code Here
+const numberGroups = ({a, b ,c}) => {
+  if(a.length > b.length && a.length > c.length){
+    return a
+  } else if(b.length > a.length && b.length > c.length){
+    return b
+  } else if(c.length > a.length && c.length > b.length)
+    return c
+}
 
 
--- nesting.js
+++ nesting.js
@@ -50,7 +50,17 @@ var employees = [
     3. Return the updated employee array.
 */
 
-//Code Here
+const employeeUpdater = () => {
+  for(let i = 0; i < employees.length; i++){
+    if(employees[i].firstName === 'Theo'){
+      employees.splice(i, 1)
+    } 
+    if(employees[i].firstName === 'Lorie'){
+      employees[i].department = 'HR'
+    }
+  }
+  return employees
+}
 
 
 
@@ -68,8 +78,17 @@ var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
     3. Return the updated array.
 */
 
-//Code Here
-
+const removeDuplicates = arr => {
+  for(let i = 0; i < arr.length; i++){
+    for(let j = i + 1; j < arr.length; j++){
+      if(arr[i] === arr[j]){
+        arr.splice(i ,1)
+        i--
+      }
+    }
+  }
+  return arr
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -97,8 +116,8 @@ var cat = {
 */
 
 //Code Here
-var grumpyActivity;
-var fluffy2ndFriend;
+var grumpyActivity = cat.catFriends[0].activities[1]
+var fluffy2ndFriend = cat.catFriends[1].name
 
 
 
@@ -138,7 +157,11 @@ var myCar = {
     3. Change atFaultForAccident from true to false.
 */
 
-//Code Here
+const recordCleaner = () => {
+    for(let i = 0; i < myCar.accidents.length; i++){
+      myCar.accidents[i].atFaultForAccident = false
+    }
+}
 
 
 
@@ -157,6 +180,16 @@ var numsArr = [ [1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
     4. Return the modified numsArr.
 */
 
-//Code Here
-
+const looper = () => {
+  for(let i = 0; i < numsArr.length; i++){
+    for(let j = 0; j < numsArr[i].length; j++){
+      if(numsArr[i][j] % 2 === 0){
+        numsArr[i][j] = 'even'
+      } else if (numsArr[i][j] % 2 !== 0){
+        numsArr[i][j] = 'odd'
+      }
+    }
+  }
+  return numsArr
+}
 
